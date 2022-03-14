b40ba8f2618ef85e45d1272ab17f30cd1a5a8139
cerp69 <cerp69@gmail.com>
2022-03-14 14:05:02 -0300

    Initial commit

---
+++ app/src/main/java/br/com/studiocr/zteste/MainActivity.java
@@ -0,0 +1,28 @@
+package br.com.studiocr.zteste;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton fab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fab = findViewById(R.id.floatbutton);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
